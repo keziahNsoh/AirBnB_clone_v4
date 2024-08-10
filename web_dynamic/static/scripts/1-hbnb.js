57fc0c5379319b931ad63fc6f40dfaeb0cd5edba
Normich ITOUA <nitoua.dev@gmail.com>
2024-08-10 13:05:29 +0100

    Fix task 3

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,14 +1,15 @@
 $(document).ready(() => {
-  const checkedAmenities = {};
+    const checkedAmenities = {};
 
-  $('.amenities input[type="checkbox"]').change(function () {
-    const amenityId = $(this).data('id');
-    const amenityName = $(this).data('name');
-    if (this.checked) {
-      checkedAmenities[amenityId] = amenityName;
-    } else {
-      delete checkedAmenities[amenityId];
-    }
-    $('.amenities h4').text(Object.values(checkedAmenities).join(', '));
+    $('.amenities input[type="checkbox"]').change(function () {
+      const amenityId = $(this).data('id');
+      const amenityName = $(this).data('name');
+      if (this.checked) {
+        checkedAmenities[amenityId] = amenityName;
+      } else {
+        delete checkedAmenities[amenityId];
+      }
+      $('.amenities h4').text(Object.values(checkedAmenities).join(', '));
+    });
   });
-});
+  
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,22 +1,33 @@
-$(document).ready(function () {
-    const apiStatusDiv = $('#api_status');
+$(document).ready(() => {
+  // Function to update API status
+  function checkApiStatus () {
+    $.ajax({
+      type: 'GET',
+      url: 'http://0.0.0.0:5001/api/v1/status/',
+      dataType: 'json',
+      success: function (response) {
+        if (response.status === 'OK') {
+          $('#api_status').addClass('available');
+        } else {
+          $('#api_status').removeClass('available');
+        }
+      }
+    });
+  }
 
-    function updateApiStatus() {
-        $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
-            if (data.status === 'OK') {
-                apiStatusDiv.addClass('available');
-            } else {
-                apiStatusDiv.removeClass('available');
-            }
-        }).fail(function () {
-            apiStatusDiv.removeClass('available');
-        });
+  // Check API status when page loads
+  checkApiStatus();
+
+  /* List checked amenitie */
+  const checkedAmenities = {};
+  $('.amenities input[type="checkbox"]').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+    if (this.checked) {
+      checkedAmenities[amenityId] = amenityName;
+    } else {
+      delete checkedAmenities[amenityId];
     }
-
-    // Initial check
-    updateApiStatus();
-
-    // Optionally, set an interval to periodically check the status
-    // setInterval(updateApiStatus, 60000); // Check every 60 seconds
+    $('.amenities h4').text(Object.values(checkedAmenities).join(', '));
+  });
 });
-
