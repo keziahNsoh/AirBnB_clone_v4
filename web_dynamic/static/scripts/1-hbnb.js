b39bc693ae7bb360e747ad2e7b06c6096aaddecd
Normich ITOUA <nitoua.dev@gmail.com>
2024-08-10 11:36:44 +0100

    Fix task 1

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,27 +1,14 @@
 $(document).ready(() => {
-    // Store selected amenities IDs in a Set to avoid duplicates
-    const selectedAmenities = new Set();
+  const checkedAmenities = {};
 
-    // Event listener for changes to checkbox inputs
-    $('input[type="checkbox"]').on('change', function() {
-        const amenityId = $(this).data('id');
-        const amenityName = $(this).data('name');
-
-        if ($(this).is(':checked')) {
-            // Add amenity ID to the set
-            selectedAmenities.add(amenityId);
-        } else {
-            // Remove amenity ID from the set
-            selectedAmenities.delete(amenityId);
-        }
-
-        // Update the h4 tag with selected amenities
-        const selectedAmenitiesNames = [];
-        $('input[type="checkbox"]:checked').each(function() {
-            selectedAmenitiesNames.push($(this).data('name'));
-        });
-
-        $('#amenities h4').text(`Amenities: ${selectedAmenitiesNames.join(', ')}`);
-    });
+  $('.amenities input[type="checkbox"]').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+    if (this.checked) {
+      checkedAmenities[amenityId] = amenityName;
+    } else {
+      delete checkedAmenities[amenityId];
+    }
+    $('.amenities h4').text(Object.values(checkedAmenities).join(', '));
+  });
 });
-
