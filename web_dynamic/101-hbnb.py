212003856d7c9636593fe0bdbf3f9842603206d2
keziahNsoh <porepeya@gmail.com>
2024-08-10 21:50:26 -0700

    finished the advance task

--- web_dynamic/101-hbnb.py
+++ web_dynamic/101-hbnb.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+""" Starts a Flask Web Application with Reviews """
 import uuid
 from models import storage
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
+from models.review import Review  # Ensure Review is imported
 from os import environ
 from flask import Flask, render_template
-app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
 
+app = Flask(__name__)
 
 @app.teardown_appcontext
 def close_db(error):
     """ Remove the current SQLAlchemy Session """
     storage.close()
 
-
-@app.route('/4-hbnb', strict_slashes=False)
+@app.route('/101-hbnb', strict_slashes=False)
 def hbnb():
-    """ HBNB is alive! """
+    """ HBNB is alive with Reviews! """
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
@@ -35,12 +33,18 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('4-hbnb.html',
+    # Fetch reviews
+    reviews = storage.all(Review).values()
+    reviews = sorted(reviews, key=lambda k: k.place_id)
+
+    return render_template('101-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places, cache_id=uuid.uuid4())
-
+                           places=places,
+                           reviews=reviews,  # Pass reviews to the template
+                           cache_id=uuid.uuid4())
 
 if __name__ == "__main__":
     """ Main Function """
     app.run(host='0.0.0.0', port=5000)
+
---
+++ web_dynamic/static/scripts/101-hbnb.js
@@ -0,0 +1,118 @@
+$(document).ready(() => {
+  const HOST = 'http://172.30.79.218';
+
+  // Function to update API status
+  function checkApiStatus() {
+    $.ajax({
+      type: 'GET',
+      url: `${HOST}:5001/api/v1/status/`,
+      dataType: 'json',
+      success: function(response) {
+        if (response.status === 'OK') {
+          $('#api_status').addClass('available');
+        } else {
+          $('#api_status').removeClass('available');
+        }
+      }
+    });
+  }
+
+  // Check API status when page loads
+  checkApiStatus();
+
+  /* List checked amenities */
+  const checkedAmenities = {};
+  $('.amenities input[type="checkbox"]').change(function() {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+    if (this.checked) {
+      checkedAmenities[amenityId] = amenityName;
+    } else {
+      delete checkedAmenities[amenityId];
+    }
+    $('.amenities h4').text(Object.values(checkedAmenities).join(', '));
+  });
+
+  // Function to create place articles
+  function createPlaceArticle(place) {
+    // Get owner info
+    let user = {};
+    $.ajax({
+      type: 'GET',
+      url: `${HOST}:5001/api/v1/users/${place.user_id}`,
+      dataType: 'json',
+      success: function(data) {
+        user = data;
+      }
+    });
+    return `
+      <article>
+        <div class="title_box">
+          <h2>${place.name}</h2>
+          <div class="price_by_night">$${place.price_by_night}</div>
+        </div>
+        <div class="information">
+          <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+          <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+          <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+        </div>
+        <div class="user">
+          <b>Owner:</b> ${user.first_name} ${user.last_name}
+        </div>
+        <div class="description">
+          ${place.description}
+        </div>
+      </article>
+    `;
+  }
+
+  // Fetch and display places
+  function fetchPlaces() {
+    const amenityIds = Object.keys(checkedAmenities);
+
+    $.ajax({
+      url: `${HOST}:5001/api/v1/places_search/`,
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({ amenities: amenityIds }),
+      success: function(places) {
+        $('section.places').empty();
+        places.forEach(function(place) {
+          $('section.places').append(createPlaceArticle(place));
+        });
+      }
+    });
+  }
+
+  // Initial fetch of places
+  fetchPlaces();
+
+  // Filter places on button click
+  $('section.filters button').click(function() {
+    fetchPlaces();
+  });
+
+  // Toggle reviews functionality
+  $('#toggle-reviews').on('click', function() {
+    const status = $(this).data('status');
+    if (status === 'show') {
+      $.ajax({
+        url: `${HOST}:5001/reviews`,
+        method: 'GET',
+        success: function(data) {
+          $('#review-list').html(data.map(review => `
+            <div class="review">
+              <h3>${review.title}</h3>
+              <p>${review.text}</p>
+            </div>
+          `).join(''));
+          $('#toggle-reviews').text('hide').data('status', 'hide');
+        }
+      });
+    } else {
+      $('#review-list').empty();
+      $('#toggle-reviews').text('show').data('status', 'show');
+    }
+  });
+});
+
